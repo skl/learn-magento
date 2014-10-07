8c3bebfdb7cfd2012c87b62caf8b6d18fea2e1ab
Stephen Lang <slang@inviqa.com>
2014-10-07 11:58:13 +0100

    3. Magento Layouts, Blocks and Templates
    - Modified index and goodbye actions to render layout
    - Installed Alanstormdotcom/Layoutviewer module to 'community' code pool
    - Root layout overridden for index and goodbye actions

--- htdocs/app/code/local/Skl/Helloworld/controllers/IndexController.php
+++ htdocs/app/code/local/Skl/Helloworld/controllers/IndexController.php
@@ -4,12 +4,14 @@ class Skl_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 'Hello index!';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
     public function goodbyeAction()
     {
-        echo 'Goodbye World!';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
     public function paramsAction()
